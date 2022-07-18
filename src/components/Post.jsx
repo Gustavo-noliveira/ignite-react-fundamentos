caa64b8ff63ed5dd8e5f2f77484dbadfbfc4d056
unknown <awguuhn@gmail.com>
2022-07-17 23:46:05 -0300

    adicionando form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,10 +21,21 @@ export function Post(){
 
       <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-      <p>👉 <a href="">jane.design/doctorcare</a></p>
+      <p>👉{'  '} <a href="">jane.design/doctorcare</a></p>
 
-      <p><a href="">#novoprojeto #nlw #rocketseat </a> </p>
+      <p><a href="">#novoprojeto #nlw #rocketseat</a> </p>
       </div>
+
+      <form className={styles.commentForm} action="">
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentario'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
